3fc8bc3381f9dbab6d2cc00a64c745641ee67db2
lsh <dltngod2000@konkuk.ac.kr>
2024-08-23 21:10:40 +0900

    aa

--- scripts/mission.py
+++ scripts/mission.py
@@ -72,20 +72,20 @@ class MissionNode:
         # 미션번호 강제할당
         #self.mission_info.mission_num = 2
 
-        if self.mission_info.mission_num == 3 and not self.is_slam_started:
-            # 1. slam 런치 파일 실행
-            launch_command = ["roslaunch", "kurrier", "kurrierSlam.launch"]
-            process = subprocess.Popen(launch_command)
-            time.sleep(6)  # 필요한 만큼 대기 (예: 다른 작업 수행)
-            self.is_slam_started = True
+        # if self.mission_info.mission_num == 3 and not self.is_slam_started:
+        #     # 1. slam 런치 파일 실행
+        #     launch_command = ["roslaunch", "kurrier", "kurrierSlam.launch"]
+        #     process = subprocess.Popen(launch_command)
+        #     time.sleep(6)  # 필요한 만큼 대기 (예: 다른 작업 수행)
+        #     self.is_slam_started = True
 
-        elif self.mission_info.mission_num != 3 and self.is_slam_started:
-            self.mission_pub.publish(self.mission_info)
-            # 2. slam 런치 파일 종료
-            #process.terminate()  # 또는 
-            process.kill()
-            process.wait()  # 프로세스가 종료될 때까지 대기
-            self.is_slam_started = False
+        # elif self.mission_info.mission_num != 3 and self.is_slam_started:
+        #     self.mission_pub.publish(self.mission_info)
+        #     # 2. slam 런치 파일 종료
+        #     #process.terminate()  # 또는 
+        #     process.kill()
+        #     process.wait()  # 프로세스가 종료될 때까지 대기
+        #     self.is_slam_started = False
 
 
 if __name__ == '__main__':
